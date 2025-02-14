f9151cf54f8dfb88d7249511f668e360a44b4fd0
Idan <Gamma-user44@greendreamteam.onmicrosoft.com>
2025-02-14 10:29:10 +0200

    feat(Expr): provide arithmetic expressions

---
+++ interpreter/Expr.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+from dataclasses import dataclass
+
+# from interpreter.token import Token
+# from interpreter.token_type import TokenType
+from Token import Token
+from token_type import TokenType
+
+type Expr = Grouping | Binary | Unary | Literal
+
+
+@dataclass
+class Grouping:
+    expression: Expr
+
+
+@dataclass
+class Binary:
+    left: Expr
+    operator: Token
+    right: Expr
+
+
+@dataclass
+class Unary:
+    operator: Token
+    expression: Expr
+
+
+@dataclass
+class Literal:
+    value: Token
+
+
+def display(expr: Expr) -> str:
+    match expr:
+        case Grouping(expr):
+            return "(group {})".format(display(expr))
+        case Binary(left, operator, right):
+            return "({} {} {})".format(display(left), operator, display(right))
+        case Unary(operator, expr):
+            return "({} {})".format(operator, display(expr))
+        case Literal(value):
+            match value.tokenType:
+                case TokenType.FALSE | TokenType.TRUE | TokenType.NIL:
+                    return str(value.tokenType)
+                case _:
+                    return str(value.literal)
