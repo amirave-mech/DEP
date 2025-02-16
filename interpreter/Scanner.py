23835a5887173bab2f35beaa88a35ee4eb2add50
Amir Rave <>
2025-02-16 19:01:38 +0200

    feat(scanner): numbers, identifiers and keywords

--- interpreter/Scanner.py
+++ interpreter/Scanner.py
@@ -1,6 +1,22 @@
 from Token import Token
 from token_type import TokenType
 
+reserved_keywords = {
+    "and": TokenType.AND,
+    "or": TokenType.OR,
+    "if": TokenType.IF,
+    "else": TokenType.ELSE,
+    "for": TokenType.FOR,
+    "while": TokenType.WHILE,
+    "print": TokenType.PRINT,
+    # Might be unnecessary
+    "var": TokenType.VAR,
+    "return": TokenType.RETURN,
+    "true": TokenType.TRUE,
+    "false": TokenType.FALSE,
+    # Do we use nil or null or something else?
+    "nil": TokenType.NIL,
+}
 
 class Scanner:
     _source: str
@@ -8,7 +24,7 @@ class Scanner:
 
     _start: int = 0
     _current: int = 0
-    _line: int = 1
+    _line: int = 0
 
     def __init__(self, source: str):
         self._source = source
@@ -77,9 +93,17 @@ class Scanner:
             case '\n':
                 self._line += 1
 
+            case '"':
+                self.string()
+
             case _:
-                # TODO: dont exit out of the loop? maybe just report the error?
-                raise Exception(self._line, "[Scanner] Unexpected Character")
+                if c.isdigit():
+                    self.number()
+                elif c.isalpha():
+                    self.identifier()
+                else:
+                    # TODO: dont exit out of the loop? maybe just report the error?
+                    raise Exception(self._line, "[Scanner] Unexpected Character")
 
     def advance(self) -> str:
         self._current += 1
@@ -107,3 +131,45 @@ class Scanner:
             return '/0'
 
         return self._source[self._current]
+
+    def peekNext(self) -> str:
+        if self._current + 1 >= len(self._source):
+            return '\0'
+
+        return self._source[self._current + 1]
+
+    def string(self) -> None:
+        while self.peek() != '\n' and self.peek() != '"' and not self.is_at_end():
+            self.advance()
+
+        if self.peek() != '"':
+            raise Exception(self._line, f'[Scanner] Unterminated string at line {self._line}')
+
+        self.advance()
+
+        value = self._source[self._start + 1: self._current - 1]
+        self.add_token(TokenType.STRING, value)
+
+    def number(self) -> None:
+        while self.peek().isdigit() and self.peek() != '\n' and not self.is_at_end():
+            self.advance()
+
+        if self.peek() == '.' and self.peekNext().isdigit():
+            self.advance()
+
+            while self.peek().isdigit():
+                self.advance()
+
+        num_str = self._source[self._start: self._current]
+        self.add_token(TokenType.NUMBER, float(num_str))
+
+    def identifier(self) -> None:
+        while self.peek().isalnum():
+            self.advance()
+
+        text = self._source[self._start: self._current]
+        # This allows every capitalization of a keyword - AND, and, And, aND
+        text = text.lower()
+        token_type = reserved_keywords[text] if text in reserved_keywords else TokenType.IDENTIFIER
+
+        self.add_token(token_type)
