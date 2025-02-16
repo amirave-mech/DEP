def35a9d869fedf9a7c7894d572c336b6e124704
IlayBit <gamma-user03@greendreamteam.onmicrosoft.com>
2025-02-16 15:22:16 +0200

    Added basic structure for getting responses from the server

--- client/dep_web/src/App.tsx
+++ client/dep_web/src/App.tsx
@@ -1,12 +1,31 @@
 import './App.css'
 
 function App() {
+  const sendCode = () => {
+    let textArea: HTMLInputElement | null = document.getElementById('code') as HTMLInputElement;
+    let serverResponse: HTMLElement | null = document.getElementById('serverResponse');
+    if (textArea && serverResponse) {
+      fetchData(textArea.value).then(data => serverResponse.textContent = (data.message + data.received) || data.error);
+    }
+  }
 
   return (
     <>
-      <h1>This is THE D.E.P Web!!!!!</h1>
+      <h1>Enter your text here</h1>
+      <textarea id='code'></textarea><br/>
+      <button onClick={sendCode}>Send Text</button><br/>
+      <p id='serverResponse'/>
     </>
   )
+}
+
+async function fetchData(text: string) {
+  try {
+    const options: RequestInit = {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(text)};
+    return fetch('http://locaclhost:5000/', options).then(response => response.json());
+  } catch (error) {
+    console.error('Error fetching data: ', error);
+  }
 }
 
 export default App
