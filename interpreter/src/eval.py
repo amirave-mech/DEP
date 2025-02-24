b52e60c41f89d491dcf77b5c64c594e77a1e27b7
Idan <Gamma-user44@greendreamteam.onmicrosoft.com>
2025-02-24 15:45:37 +0200

    feat(eval): evaluation of arithmetic and string operations

---
+++ interpreter/src/eval.py
@@ -0,0 +1,120 @@
+import src.expr as expr
+from src.expr import Expr
+from src.Token import Token
+from src.token_type import TokenType
+
+
+type Literal = str | float | bool
+
+# TODO: Should `Token` hold booleans as literals and not just token types?
+
+
+# NOTE: While this class currently acts as a namespace, instance-based state will be held in later stages
+class Eval:
+    @staticmethod
+    def expression(ast: Expr) -> Literal:
+        match ast:
+            case expr.Literal():
+                return Eval.__visit_literal(ast)
+            case expr.Grouping():
+                return Eval.__visit_grouping(ast)
+            case expr.Unary():
+                return Eval.__visit_unary(ast)
+            case expr.Binary():
+                return Eval.__visit_binary(ast)
+
+    # Expression Visitors
+    @staticmethod
+    def __visit_literal(expr: expr.Literal) -> Literal:
+        # print(type(expr.value.literal))
+        if expr.value.literal is None:
+            raise Exception("Unexpected non-literal token: {}".format(expr.value))
+
+        match expr.value.literal:
+            case str() | bool() | float():
+                return expr.value.literal
+            case int():
+                return float(expr.value.literal)
+            case _:
+                raise Exception(
+                    "Literal token of unexpected type: {}".format(expr.value)
+                )
+
+    @staticmethod
+    def __visit_grouping(expr: expr.Grouping) -> Literal:
+        return Eval.expression(expr.expression)
+
+    @staticmethod
+    def __visit_unary(expr: expr.Unary) -> Literal:
+        # TODO: Verify type safety of `value`
+        value = Eval.expression(expr.expression)
+
+        match expr.operator:
+            case TokenType.MINUS:
+                if not isinstance(value, float):
+                    raise Exception(Eval.__format_invalid_literal(TokenType.MINUS))
+                value = -value
+            case TokenType.BANG:
+                if not isinstance(value, bool):
+                    raise Exception(Eval.__format_invalid_literal(TokenType.BANG))
+                # The language is strictly typed - the negation operator only works on booleans
+                value = not value
+            case _:
+                raise Exception("Unexpected unary operation: {}".format(expr.operator))
+
+        return value
+
+    @staticmethod
+    def __visit_binary(expr: expr.Binary) -> Literal:
+        left = Eval.expression(expr.left)
+        right = Eval.expression(expr.right)
+
+        match expr.operator.tokenType:
+            case TokenType.MINUS:
+                if not isinstance(left, float) or isinstance(right, float):
+                    raise Exception(Eval.__format_invalid_literal(TokenType.MINUS))
+                return float(left) - float(right)
+            case TokenType.SLASH:
+                if not isinstance(left, float) or isinstance(right, float):
+                    raise Exception(Eval.__format_invalid_literal(TokenType.SLASH))
+                return float(left) / float(right)
+            case TokenType.STAR:
+                if not isinstance(left, float) or isinstance(right, float):
+                    raise Exception(Eval.__format_invalid_literal(TokenType.STAR))
+                return float(left) * float(right)
+            case TokenType.PLUS:
+                if isinstance(left, float) and isinstance(right, float):
+                    return left + right
+                elif isinstance(left, str) and isinstance(right, str):
+                    return left + right
+                else:
+                    raise Exception(Eval.__format_invalid_literal(TokenType.PLUS))
+            case TokenType.GREATER:
+                if not isinstance(left, float) or isinstance(right, float):
+                    raise Exception(Eval.__format_invalid_literal(TokenType.GREATER))
+                return float(left) > float(right)
+            case TokenType.GREATER_EQUAL:
+                if not isinstance(left, float) or isinstance(right, float):
+                    raise Exception(
+                        Eval.__format_invalid_literal(TokenType.GREATER_EQUAL)
+                    )
+                return float(left) >= float(right)
+            case TokenType.LESS:
+                if not isinstance(left, float) or isinstance(right, float):
+                    raise Exception(Eval.__format_invalid_literal(TokenType.LESS))
+                return float(left) < float(right)
+            case TokenType.LESS_EQUAL:
+                if not isinstance(left, float) or isinstance(right, float):
+                    raise Exception(Eval.__format_invalid_literal(TokenType.LESS_EQUAL))
+                return float(left) <= float(right)
+            case TokenType.EQUAL_EQUAL:
+                return left == right
+            case TokenType.BANG_EQUAL:
+                return left == right
+            case _:
+                raise Exception("Invalid binary operation: {}".format(expr.operator))
+
+    @staticmethod
+    def __format_invalid_literal(op_type: TokenType) -> str:
+        # TODO: Provide information about which literal token is invalid
+        return "{} operator applied on an invalid literal type".format(op_type)
